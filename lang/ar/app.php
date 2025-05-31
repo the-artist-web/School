bb21fe22934adb40c0c91e88be7b9c5742b34031
the artist web <teamtheartistweb@gmail.com>
2025-05-31 12:48:05 +0300

    finished

--- lang/ar/app.php
+++ lang/ar/app.php
@@ -1,14 +1,118 @@
 <?php
 
 return [
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
+    /**
+     * HEADER
+     */
+    "login" => "تسجيل الدخول",
+    "create_a_new_account" => "إنشاء حساب جديد",
+
+    /**
+     * HOME PAGE 
+     */
+
+    // SECTION TWO
+    "choose_teachers" => "اختر المعلمين",
+    "view_more_teachers" => "عرض المزيد من المعلمين",
+
+    // SECTION THREE
+    "our_suggested_courses" => "كورسات مقترحة لك",
+    "view_more_courses" => "عرض المزيد من الكورسات",
+    "entry_to_the_course" => "دخول الكورس",
+    "subscribe_now" => "اشترك الآن",
+    "egp" => "ج.م",
+
+    // SECTION FOUR
+    "browse_courses" => "تصفح الكورسات",
+    
+    // SECTION FIVE
+    "materials_section" => "قسم المواد",
+
+    // SECTION SIX
+    "how_section" => "قسم كيف يعمل",
+
+    // SECTION SEVEN
+    "your_chance_has_come" => "فرصتك قد حانت",
+
+    // SECTION EIGHT
+    "let_me_simplify_this" => "دعني أبسط لك الأمور",
+
+    // SECTION NINE
+    "start_now" => "ابدأ الآن",
+
+    // FOOTER
+    "hotline" => "الخط الساخن:",
+
+    /**
+     * AUTH
+     */
+
+    // LOGIN
+    "phone_number" => "رقم الهاتف",
+    "password" => "كلمة المرور",
+    "or_log_in_using_the_code" => "أو سجل الدخول باستخدام الكود",
+    "from_here" => "من هنا",
+    "forgot_your_password_click_here" => "هل نسيت كلمة المرور؟ اضغط هنا",
+    "dont_have_an_account_create_one_now" => "ليس لديك حساب؟ أنشئ حساباً الآن!",
+
+    // CODE
+    "enter_the_code" => "أدخل الكود",
+    "re_enter_the_code" => "أعد إدخال الكود",
+
+    // REGISTER
+    "request_create_an_account" => "طلب إنشاء حساب:",
+    "first_name" => "الاسم الأول",
+    "second_name" => "الاسم الثاني",
+    "third_name" => "الاسم الثالث",
+    "last_name" => "الاسم الأخير",
+    "fathers_phone_number" => "رقم هاتف الأب",
+    "mothers_phone_number" => "رقم هاتف الأم",
+    "school_name" => "اسم المدرسة",
+    "email_address" => "البريد الإلكتروني",
+    "confirm_password" => "تأكيد كلمة المرور",
+    "create_an_account" => "إنشاء حساب",
+    "already_have_an_account_log_in_now" => "لديك حساب بالفعل؟ سجل الدخول الآن!",
+
+    /**
+     * COURSE PAGE
+     */
+    "course_creation_date" => "تاريخ إنشاء الكورس",
+    "latest_course_update" => "أحدث تحديث للكورس",
+    "book_your_session_appointment" => "احجز موعد جلستك",
+    "fairy" => "الجنيّة", // أو حسب السياق يمكن تغييره
+    "subscribe_now" => "اشترك الآن",
+    "course_content" => "محتوى الكورس",
+
+    /**
+     * FILTER
+     */
+    "choose_the_academic_year" => "اختر السنة الدراسية",
+    "choose_the_teacher_whose_courses_you_want" => "اختر المعلم الذي تريد كورساته",
+    "select_the_subject" => "اختر المادة",
+    "search" => "بحث",
+
+    /**
+     * SEARCH
+     */
+    "you_searched_for" => "لقد بحثت عن:",
+    "subscriptions_monthly" => "الاشتراكات الشهرية",
+    "courses_separate" => "كورسات منفصلة",
+
+    /**
+     * STORE LOCATOR
+     */
+    "the_knowledge_is_yours_and_we_made_it_easy_for_you" => "المعرفة لك... وقد سهّلناها عليك! 😉💡",
+    "governorate" => "المحافظة",
+    "show_more" => "عرض المزيد",
+
+    /**
+     * TEACHER
+     */
+    "materials" => "المواد",
+    "courses" => "الكورسات",
+    "teacher" => "المعلم",
+    "number_of_subjects" => "عدد المواد",
+    "number_of_courses" => "عدد الكورسات",
+    "separate_courses_for_the_teacher" => "كورسات منفصلة للمعلم",
+    "teachers_monthly_subscription" => "اشتراك شهري للمعلم",
 ];
--- lang/en/app.php
+++ lang/en/app.php
@@ -1,14 +1,118 @@
 <?php
 
 return [
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
-    "" => "",
+    /**
+     * HEADER
+     */
+    "login" => "Login",
+    "create_a_new_account" => "Create a new account",
+
+    /**
+     * HOME PAGE 
+     */
+
+    // SECTION TWO
+    "choose_teachers" => "Choose teachers",
+    "view_more_teachers" => "View more teachers",
+
+    // SECTION THREE
+    "our_suggested_courses" => "Our suggested courses",
+    "view_more_courses" => "View more courses",
+    "entry_to_the_course" => "Entry to the course",
+    "subscribe_now" => "Subscribe now",
+    "egp" => "EGP",
+
+    // SECTION FOUR
+    "browse_courses" => "Browse courses",
+    
+    // SECTION FIVE
+    "materials_section" => "Materials Section",
+
+    // SECTION SEX
+    "how_section" => "How Section",
+
+    // SECTION SEVEN
+    "your_chance_has_come" => "Your chance has come",
+
+    // SECTION EIGHT
+    "let_me_simplify_this" => "Let me simplify this",
+
+    // SECTION NINE
+    "start_now" => "Start now",
+
+    // FOOTER
+    "hotline" => "Hotline:",
+
+    /**
+     * AUTH
+     */
+
+    // LOGIN
+    "phone_number" => "Phone Number",
+    "password" => "Password",
+    "or_log_in_using_the_code" => "Or log in using the code",
+    "from_here" => "From here",
+    "forgot_your_password_click_here" => "Forgot your password? Click here",
+    "dont_have_an_account_create_one_now" => "Don't have an account? Create one now!",
+
+    // CODE
+    "enter_the_code" => "Enter The Code",
+    "re_enter_the_code" => "Re-Enter The Code",
+
+    // REGISTER
+    "request_create_an_account" => "Request Create an account:",
+    "first_name" => "First Name",
+    "second_name" => "Second Name",
+    "third_name" => "Third Name",
+    "last_name" => "Last Name",
+    "fathers_phone_number" => "Father's Phone Number",
+    "mothers_phone_number" => "Mother's Phone Number",
+    "school_name" => "School Name",
+    "email_address" => "Email Address",
+    "confirm_password" => "Confirm Password",
+    "create_an_account" => "Create an account",
+    "already_have_an_account_log_in_now" => "Already have an account? Log in now!",
+
+    /**
+     * COURSE PAGE
+     */
+    "course_creation_date" => "Course creation date",
+    "latest_course_update" => "Latest course update",
+    "book_your_session_appointment" => "Book your session appointment",
+    "fairy" => "fairy",
+    "subscribe_now" => "Subscribe now",
+    "course_content" => "Course content",
+
+    /**
+     * FILTER
+     */
+    "choose_the_academic_year" => "Choose the academic year",
+    "choose_the_teacher_whose_courses_you_want" => "Choose the teacher whose courses you want",
+    "select_the_subject" => "Select the subject",
+    "search" => "Search",
+
+    /**
+     * SEARCH
+     */
+    "you_searched_for" => "You searched for:",
+    "subscriptions_monthly" => "Subscriptions monthly",
+    "courses_separate" => "Courses separate",
+
+    /**
+     * STORE LOCATOR
+     */
+    "the_knowledge_is_yours_and_we_made_it_easy_for_you" => "The knowledge is yours... and we made it easy for you! 😉💡",
+    "governorate" => "governorate",
+    "show_more" => "Show more",
+
+    /**
+     * TEATCHER
+     */
+    "materials" => "Materials",
+    "courses" => "Courses",
+    "teacher" => "teacher",
+    "number_of_subjects" => "Number of subjects",
+    "number_of_courses" => "Number of courses",
+    "separate_courses_for_the_teacher" => "Separate courses for the teacher",
+    "teachers_monthly_subscription" => "Teacher's monthly subscription",
 ];
